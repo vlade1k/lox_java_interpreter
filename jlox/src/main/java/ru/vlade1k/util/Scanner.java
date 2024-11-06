dc550a11d30c7e2aaf8b32a687f37703c8265a08
vlade1k <vladode2015@yandex.ru>
2024-11-06 21:01:01 +0300

    add support of all Lox tokens

--- jlox/src/main/java/ru/vlade1k/util/Scanner.java
+++ jlox/src/main/java/ru/vlade1k/util/Scanner.java
@@ -1,34 +1,15 @@
 package ru.vlade1k.util;
 
-import static ru.vlade1k.token.TokenType.BANG;
-import static ru.vlade1k.token.TokenType.BANG_EQUAL;
-import static ru.vlade1k.token.TokenType.COMMA;
-import static ru.vlade1k.token.TokenType.DOT;
-import static ru.vlade1k.token.TokenType.EOF;
-import static ru.vlade1k.token.TokenType.EQUAL;
-import static ru.vlade1k.token.TokenType.EQUAL_EQUAL;
-import static ru.vlade1k.token.TokenType.GREATER;
-import static ru.vlade1k.token.TokenType.GREATER_EQUAL;
-import static ru.vlade1k.token.TokenType.LEFT_BRACE;
-import static ru.vlade1k.token.TokenType.LEFT_PAREN;
-import static ru.vlade1k.token.TokenType.LESS;
-import static ru.vlade1k.token.TokenType.LESS_EQUAL;
-import static ru.vlade1k.token.TokenType.MINUS;
-import static ru.vlade1k.token.TokenType.NUMBER;
-import static ru.vlade1k.token.TokenType.PLUS;
-import static ru.vlade1k.token.TokenType.RIGHT_BRACE;
-import static ru.vlade1k.token.TokenType.RIGHT_PAREN;
-import static ru.vlade1k.token.TokenType.SEMICOLON;
-import static ru.vlade1k.token.TokenType.SLASH;
-import static ru.vlade1k.token.TokenType.STAR;
-import static ru.vlade1k.token.TokenType.STRING;
+import static ru.vlade1k.token.TokenType.*;
 
 import ru.vlade1k.JLoxInterpreter;
 import ru.vlade1k.token.Token;
 import ru.vlade1k.token.TokenType;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
   private final String source;
@@ -36,6 +17,28 @@ public class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
 
   public Scanner(String source) {
     this.source = source;
@@ -105,10 +108,17 @@ public class Scanner {
       case '\n':
         line++;
         break;
+      case 'o' :
+        if (match('r')) {
+          addToken(OR);
+        }
+        break;
       case '"': string(); break;
       default:
         if (isDigit(c)) {
           number();
+        } else if (isAlpha(c)) {
+          identifier();
         } else {
           JLoxInterpreter.error(line, "Unexpected character");
         }
@@ -135,6 +145,31 @@ public class Scanner {
     addToken(STRING, value);
   }
 
+  private void identifier() {
+    while(isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) {
+      type = IDENTIFIER;
+    }
+    addToken(type);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+           (c == '_');
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
   private void number() {
     while(isDigit(peek())) advance();
 
@@ -144,6 +179,14 @@ public class Scanner {
     }
 
     addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    advance();
+    return true;
   }
 
   private char peekNext() {
@@ -158,17 +201,5 @@ public class Scanner {
 
   private boolean isAtEnd() {
     return current >= source.length();
-  }
-
-  private boolean isDigit(char c) {
-    return c >= '0' && c <= '9';
-  }
-
-  private boolean match(char expected) {
-    if (isAtEnd()) return false;
-    if (source.charAt(current) != expected) return false;
-
-    advance();
-    return true;
   }
 }
