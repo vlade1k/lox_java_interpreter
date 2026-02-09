d9e6dd4f906e80dec1d899413c8ce3ed8087d188
vlade1k <vladode2015@yandex.ru>
2026-02-09 22:11:58 +0300

    test: add scanner test for all supported token types

---
+++ jlox/src/test/java/ru/vlade1k/scanner/AllSupportedTokensTest.java
@@ -0,0 +1,36 @@
+package ru.vlade1k.scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import ru.vlade1k.scanner.token.Token;
+import ru.vlade1k.scanner.token.TokenType;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.Set;
+
+public class AllTokensTest extends ScannerAbstractTest {
+  private static final EnumSet<TokenType> NOT_SUPPORTED_TOKENS = EnumSet.of(
+      TokenType.CLASS,
+      TokenType.SUPER,
+      TokenType.THIS,
+      TokenType.DOT
+  );
+
+  private static final Set<TokenType>
+      SUPPORTED_TOKEN_TYPES = EnumSet.copyOf(Arrays.stream(TokenType.values())
+                                              .filter(tokenType -> !NOT_SUPPORTED_TOKENS.contains(tokenType))
+                                              .toList());
+
+  protected AllTokensTest() throws IOException {
+    super("all-tokens.lox");
+  }
+
+  @Test
+  void hasAllSupportedTokenTypes() {
+    var actualTokenTypes = EnumSet.copyOf(scanner.scanTokens().stream().map(Token::getType).toList());
+    assertEquals(SUPPORTED_TOKEN_TYPES, actualTokenTypes);
+  }
+}
